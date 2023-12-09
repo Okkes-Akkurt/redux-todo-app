74e896407f96117d05164d199d7902190087e2d7
Ökkeş Akkurt <okkesakkurt46@gmail.com>
2023-12-09 16:17:10 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='info'>
+			<p>Click to edit a todo</p>
+			<p>
+				Created by <a href='https://d12n.me/'>Dmitry Sharabin</a>
+			</p>
+			<p>
+				Coding by <a href='https://www.linkedin.com/in/okkes-akkurt/'>Okkes Akkurt</a>
+			</p>
+			<p>
+				Part of <a href='http://todomvc.com'>TodoMVC</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
